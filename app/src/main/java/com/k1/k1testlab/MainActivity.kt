8871b6813dfa1b31a33fd2fb2a35b9bd5ac151a6
lijiaqi.k1 <lijiaqi.k1@bytedance.com>
2021-09-17 11:19:05 +0800

    first commit
    Change-Id: Id635cd2290dc4afbbb1076c5001b3631cf0777e4

---
+++ app/src/main/java/com/k1/k1testlab/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.k1.k1testlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
